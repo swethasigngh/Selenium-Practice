32ad1bfb5fc9fac981108676e843918dfa5a4cf5
Shweta Singh <shwetasingh.6541@gmail.com>
2019-09-21 11:43:40 -0500

    report chages

--- TestNGExamples/src/com/swetha/framework/Newid.java
+++ TestNGExamples/src/com/swetha/framework/Newid.java
@@ -10,34 +10,38 @@ import org.testng.annotations.Test;
 
 public class Newid {
 	WebDriver driver;
-	
+
 	@BeforeSuite
 	public void launchBrowser() {
-	
-	System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-	driver = new ChromeDriver();
-	driver.manage().window().maximize();
-}
+
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+
 	@BeforeTest
 	public void enterURL() {
-	driver.get("https://reg.ebay.com/reg/PartialReg?ru=https%3A%2F%2Fwww.ebay.com%2F");
+		driver.get("https://reg.ebay.com/reg/PartialReg?ru=https%3A%2F%2Fwww.ebay.com%2F");
 
 	}
+
 	@Test
-	public void enterUserName() {	
-	driver.findElement(By.name("firstname")).sendKeys("shweta");
+	public void enterUserName() {
+		driver.findElement(By.name("firstname")).sendKeys("shweta");
 	}
+
 	@Test
 	public void LastrName() {
-driver.findElement(By.name("lastname")).sendKeys("Singh");
+		driver.findElement(By.name("lastname")).sendKeys("Singh");
 	}
 
-@Test
-public void rName() {
-driver.findElement(By.linkText("AdChoice")).click();
-}
+	@Test
+	public void rName() {
+		driver.findElement(By.linkText("AdChoice")).click();
+	}
+
 	@AfterSuite
 	public void closeBrowser() {
-	driver.close();
+		driver.close();
 	}
-	}
+}
